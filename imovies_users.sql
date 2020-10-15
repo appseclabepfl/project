61287151f767b435e25c40eb729589028a6585be
Simon <simon.perriard@epfl.ch>
2020-10-15 21:18:36 +0200

    Full DB config on imovies_user.sql

--- imovies_users.sql
+++ imovies_users.sql
@@ -52,4 +52,32 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-10-28 16:53:06
+
+-- USERS AND PERMISSIONS
+
+USE `mysql`;
+
+--
+-- Clean old users and permissions
+--
+DROP USER IF EXISTS `coreca`@`10.10.10.3`;
+DROP USER IF EXISTS `backup`@`10.10.10.4`;
+DROP USER IF EXISTS `webserver`@`10.10.20.2`;
+
+--
+-- Create users, bind them to their address and grant permissions
+--
+CREATE USER `coreca`@`10.10.10.3` IDENTIFIED BY 'database';
+CREATE USER `backup`@`10.10.10.4` IDENTIFIED BY 'database';
+CREATE USER `webserver`@`10.10.20.2` IDENTIFIED BY 'database';
+
+REVOKE ALL PRIVILEGES ON *.* FROM `coreca`@`10.10.10.3`;
+REVOKE ALL PRIVILEGES ON *.* FROM `backup`@`10.10.10.4`;
+REVOKE ALL PRIVILEGES ON *.* FROM `webserver`@`10.10.20.2`;
+
+GRANT SELECT ON `imovies_users`.`users` TO `coreca`@`10.10.10.3`;
+GRANT CREATE ON *.* TO `backup`@`10.10.10.4`;
+GRANT INSERT ON `imovies_users`.`users` TO `backup`@`10.10.10.4`;
+GRANT SELECT, UPDATE ON `imovies_users`.`users` TO `webserver`@`10.10.20.2`;
+
+FLUSH PRIVILEGES;
